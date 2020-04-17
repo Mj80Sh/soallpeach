ccd8ac76f59df7e3fa47650d4a0b1799350311e1
mjshafiei1380 <mjsh1385@gmail.com>
2020-04-17 21:50:21 +0430

    edited algorithem

--- prime/main.c
+++ prime/main.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define MAX_VAL 99992
-int m;
-int num;
-int primes[MAX_VAL];
+#define MAX_VAL 99999
+unsigned long int m;
+unsigned long int num;
+unsigned long int primes[MAX_VAL];
 
 typedef enum
 {
@@ -15,14 +15,14 @@ typedef enum
 
 
 
-int is_prime(int n)
+unsigned long int is_prime(unsigned long int n)
 {
     if (n <= 3)
         return 1;
     if (n % 2 == 0 || n % 3 == 0)
         return 0;
 
-    for (m = 5; m < (int)sqrt(n) + 1; m = m + 6)
+    for (m = 5; m < (unsigned long int)sqrt(n) + 1; m = m + 6)
     {
         if (n % m == 0 || n % (m + 2) == 0)
             return 0;
@@ -30,22 +30,22 @@ int is_prime(int n)
     return 1;
 }
 
-void SieveOfAtkin(int lmt)
+void SieveOfAtkin(bool *sieve ,unsigned long int lmt)
 {
     primes[2]=1;
     primes[3]=1;
     primes[4]=0;
     primes[5]=1;
     primes[1]=0;
-    bool sieve[lmt];
-    for (int i = 0; i < lmt; i++)
+
+    for (unsigned long int i = 0; i < lmt; i++)
         sieve[i] = false;
-    for (int a = 1; a * a < lmt; a++)
+    for (unsigned long int a = 1; a * a < lmt; a++)
     {
-        for (int b = 1; b * b < lmt; b++)
+        for (unsigned long int b = 1; b * b < lmt; b++)
         {
             // Main part of Sieve of Atkin
-            int n = (4 * a* a) + (b * b);
+            unsigned long int n = (4 * a* a) + (b * b);
             if (n <= lmt && (n % 12 == 1 || n % 12 == 5))
                 sieve[n] ^= true;
             n = (3 * a * a) + (b * b);
@@ -56,29 +56,29 @@ void SieveOfAtkin(int lmt)
                 sieve[n] ^= true;
         }
     }
-    for (int r = 5; r * r < lmt; r++)
+    for (unsigned long int r = 5; r * r < lmt; r++)
     {
         if (sieve[r])
         {
-            for (int i = r * r; i < lmt; i += r * r)
+            for (unsigned long int i = r * r; i < lmt; i += r * r)
                 sieve[i] = false;
         }
     }
-    for (int x = 5; x < lmt; x++)
-        primes[x]=sieve[x];
+
 }
 
-int main(int argc, char *argv[])
+unsigned long int main(unsigned long int argc, char *argv[])
 {
     FILE *file = fopen(argv[1], "r");
     char buf[1000];
-    SieveOfAtkin(MAX_VAL);
+    bool sieve[MAX_VAL];
+    SieveOfAtkin(sieve,MAX_VAL);
     while (fgets(buf, 1000, file) != NULL)
     {
 
         num = atoi(buf);
         if(num<=MAX_VAL)
-            printf("%d\n", primes[num]);
+            printf("%d\n", sieve[num]);
         else
             printf("%d\n", is_prime(num));
 
